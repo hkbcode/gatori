b1d0201da3a837dc7544b1f66c565cbbb02c4cce
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-04-04 22:23:31 +0530

    initial commit :fire:

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Button } from 'antd'
+
+
+const indexPage = () => {
+  return (
+    <div style={{ marginLeft: 500, marginTop: 100 }}>
+      <h1>Build some awesome website with </h1>
+      <Button type="primary" style={{ marginLeft: 150 }}> Ant design + Gatsbyjs </Button>
+    </div>
+  )
+}
+
+
+export default indexPage
