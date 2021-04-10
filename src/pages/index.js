0e8a868da574bcce6175a0de580ae1d481c95e59
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-04-10 17:50:01 +0530

    edit home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Button } from 'antd'
 const indexPage = () => {
   return (
     <div style={{ marginLeft: 500, marginTop: 100 }}>
-      <h1>Build some awesome website with </h1>
+      <h1>Build awesome website with </h1>
       <Button type="primary" style={{ marginLeft: 150 }}> Ant design + Gatsbyjs </Button>
     </div>
   )
